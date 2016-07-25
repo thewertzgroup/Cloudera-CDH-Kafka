072844a08ebb13740a080e3f20e97a1975954c03
TheWertzGroup <christoph@cloudera.com>
2016-07-25 15:11:23 -0600

    Driver shell. cw

---
+++ src/main/java/com/ztrew/kafka/driver/Driver.java
@@ -0,0 +1,10 @@
+package com.ztrew.kafka.driver;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(Driver.class);
+	}
+
+}
